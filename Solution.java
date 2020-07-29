7d08baa294b8d21e83a26bb0e3cf58e5fd2b10bf
ShaniyaS13 <57805735+ShaniyaS13@users.noreply.github.com>
2020-07-29 11:27:11 -0400

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,22 +1,21 @@
 import java.util.Scanner;
 import java.math.*;
 public class Solution {
-	public int reverse(int x) {
-		boolean negative = x < 0;
-		if(x < 0) {
-			x *= -1;
-		}
-		long reversed = 0;
-		while(x > 0) {
-			reversed = (reversed * 10) + (x %10);
-			x /= 10;
-		}
-		if(reversed > Integer.MAX_VALUE) {
-			return 0;
-		}
-		return (int) (negative ? (reversed * -1): reversed);
-		
-	}
+	public int reverse(int n) {
+		long rev = 0;
+		boolean negative = n < 0;
+		if(n < 0) {
+				n = n * -1;
+				 }
+		while(n > 0) {
+			rev = (rev * 10) + (n % 10);
+			n = n / 10;
+					}
+		if(rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE) {
+					return 0;
+				}
+				return (int) (negative ? (rev * -1) : rev);
+		    }
 	 
 	 public static void main(String[] args) {
 		 Scanner input = new Scanner(System.in);
